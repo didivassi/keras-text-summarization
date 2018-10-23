647b3b5e9561f81bcbb5fe77a995461fda399728
Diogo Velho <diogovelho@gmail.com>
2018-10-23 19:24:46 +0100

    Update seq2seq.py

--- keras_text_summarization/library/seq2seq.py
+++ keras_text_summarization/library/seq2seq.py
@@ -41,9 +41,8 @@ class Seq2SeqSummarizer(object):
         encoder_outputs, encoder_state_h, encoder_state_c = encoder_lstm(encoder_embedding(encoder_inputs))
         encoder_states = [encoder_state_h, encoder_state_c]
 
-        decoder_inputs = Input(shape=(None, ), name='decoder_inputs')
-        decoder_embedding = Embedding(input_dim=self.num_target_tokens , output_dim=HIDDEN_UNITS,
-                                      input_length=self.max_target_seq_length, name='decoder_embedding')
+        decoder_inputs = Input(shape=(None,self.num_target_tokens ), name='decoder_inputs')
+        decoder_embedding = Embedding(self.num_target_tokens ,self.max_target_seq_length, name='decoder_embedding')
         
 
         
